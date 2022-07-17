419d1ba5376a6f19caed382f3650997e37f05a79
dori-dev <mr.dori.dev@gmail.com>
2022-07-17 10:41:05 +0430

    add pagination to apis

--- airplanes/settings.py
+++ airplanes/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django rest framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 8
+}
