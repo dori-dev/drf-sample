9985fc2b68a23e64b98e2926221d42cbdb9f3c0a
dori-dev <mr.dori.dev@gmail.com>
2022-07-17 11:05:47 +0430

    add filtering to plane api

--- airplanes/settings.py
+++ airplanes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'planes',
 ]
 
@@ -128,5 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 8
+    'PAGE_SIZE': 8,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
+
 }
--- planes/views.py
+++ planes/views.py
@@ -1,6 +1,8 @@
-from rest_framework.generics import (
-    RetrieveUpdateDestroyAPIView, ListCreateAPIView)
+# from rest_framework.generics import (
+#     RetrieveUpdateDestroyAPIView, ListCreateAPIView)
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from planes.models import Plane
 from planes.serializers import PlaneSerializer
 
@@ -18,3 +20,14 @@ from planes.serializers import PlaneSerializer
 class PlaneViewSet(ModelViewSet):
     queryset = Plane.objects.all()
     serializer_class = PlaneSerializer
+    filter_backends = [
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = ['plane_from', 'plane_to']
+    search_fields = ['plane_from', 'plane_to']
+    ordering_fields = [
+        'id', 'capacity',
+        'plane_time', 'arrive_time'
+    ]
