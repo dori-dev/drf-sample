e4ba368e9d81ef55cccb48b521cedd1860f7ccd2
dori-dev <mr.dori.dev@gmail.com>
2022-07-17 09:01:42 +0430

    create plane model and register plane model in admin

--- planes/admin.py
+++ planes/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from planes.models import Plane
+
+
+class PlaneAdmin(admin.ModelAdmin):
+    list_display = ('plane_from', 'plane_to', 'plane_time',
+                    'arrive_time', 'capacity')
+
+
+admin.site.register(Plane, PlaneAdmin)
--- planes/models.py
+++ planes/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Plane(models.Model):
+    plane_from = models.CharField(
+        verbose_name='from',
+        max_length=255)
+    plane_to = models.CharField(
+        verbose_name='to',
+        max_length=255)
+    plane_time = models.DateTimeField()
+    arrive_time = models.DateTimeField()
+    capacity = models.SmallIntegerField()
