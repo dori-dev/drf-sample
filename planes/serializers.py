318817d2262990200d64752cfaf34d9957e8ce9f
dori-dev <mr.dori.dev@gmail.com>
2022-07-17 09:02:35 +0430

    create planes serializer

---
+++ planes/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from planes.models import Plane
+
+
+class PlaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plane
+        fields = ('plane_from', 'plane_to', 'plane_time',
+                  'arrive_time', 'capacity')
