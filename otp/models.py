42d59ab65ffafb8af9bf0cdf09ef4a29620d0013
dori-dev <mr.dori.dev@gmail.com>
2022-07-20 21:02:26 +0430

    add profile model and using signals

--- otp/models.py
+++ otp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
@@ -7,3 +9,14 @@ class User(AbstractUser):
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)
+    birth_date = models.DateTimeField(null=True)
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
