57578758a1ebcd393b2befbaf5bfa6a31afa9621
dori-dev <mr.dori.dev@gmail.com>
2022-07-20 21:14:42 +0430

    register models to admin

--- otp/admin.py
+++ otp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from otp.models import User, Profile, OtpRequest
+
+
+class OtpRequestAdmin(admin.ModelAdmin):
+    list_display = ['phone', 'channel', 'password']
+    list_filter = ['phone', 'channel']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'birth_date']
+
+
+admin.site.register(User)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(OtpRequest, OtpRequestAdmin)
